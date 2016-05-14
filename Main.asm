96e2df9592beab1763dfdbb7144d63b9430a67d1
hang-on <asje@me.com>
2016-05-14 07:09:34 +0200

    Break stuff up

--- Main.asm
+++ Main.asm
@@ -2,7 +2,7 @@
 
 .bank 0 slot 0
   .include "Spritelib.inc"
-  .include "Objectlib.inc"
+  .include "Objects/Objectlib.inc"
 
 .bank 1 slot 1
   .include "Objects/Object-Data.inc"
@@ -12,142 +12,23 @@
 .ramsection "Main variables" slot 3
   FrameCounter db
   PlayerHandle db
-  GargoyleHandle db
-  Zombie1Handle db
-  NextEventIndex db
 .ends
 ; -----------------------------------------------------------------------------
 .section "Main" free
 ; -----------------------------------------------------------------------------
 
-
-  ZombieTiles:
-    .include "Objects/Zombie-Tiles.inc"
-  ZombieTilesEnd:
-
   BatchLoadTable:
     .db (BatchLoadTableEnd-BatchLoadTable-1)/6
-    .dw SwabbyTiles PLAYER_1_TILES_START SwabbyTilesEnd-SwabbyTiles
-    .dw GargoyleTiles ENEMY_1_TILES_START GargoyleTilesEnd-GargoyleTiles
-    .dw ZombieTiles ENEMY_2_TILES_START ZombieTilesEnd-ZombieTiles
+    .dw SwabbyTiles $2000 SwabbyTilesEnd-SwabbyTiles
+    .dw GargoyleTiles $2400 GargoyleTilesEnd-GargoyleTiles
   BatchLoadTableEnd:
-
-  ; Tilebank 2 map:
-  .equ PLAYER_1_TILES_START $2000
-  .equ ENEMY_1_TILES_START $2400
-  .equ ENEMY_2_TILES_START $2600
-
-  ; Make character code indexes for the meta sprite data blocks.
-  .equ P1 (PLAYER_1_TILES_START-$2000)/32
-  .equ E1 (ENEMY_1_TILES_START-$2000)/32
-  .equ E2 (ENEMY_2_TILES_START-$2000)/32
-
-  Make3x2MetaSprites P1, Swabby1:, P1+6, Swabby2:, P1+12, Swabby3:
-  Make2x2MetaSprites E1, Gargoyle1:, E1+4, Gargoyle2:
-  Make2x3MetaSprites E2, Zombie1:, E2+6, Zombie2:
-
-  ; Animation table: An array of metasprites with corresponding timer values.
-  SwabbyFlying:
-    .dw $2000 Swabby1 $2000 Swabby3 $2000 Swabby1 $2000 Swabby3
-    .dw $2000 Swabby2 $20ff Swabby3 ; MSB $ff (or non-zero value) means loop.
-
-  ; Movement patterns.
-  Pattern1: ; The chineese bird-men.
-    .db 0, -1, 70, 1, -1, 90, 0, 1, 175, SELF_DESTRUCT
-
-  Pattern2: ; Classic sine-wave.
-    .db 1, -1, 32, -1, -1, 64, 1, -1, 32, SELF_DESTRUCT
-
-  Pattern3: ; The chineese bird-men, with delay change.
-    .db 0, -1, 70
-    .db SET_DELAY, 2
-    .db 1, -1, 90,
-    .db SET_DELAY, 1
-    .db 0, 1, 175
-    .db SELF_DESTRUCT
-
-  Pattern4: ; Neo sine-wave.
-    .db 2, -1, 16, -2, -1, 32, 2, -1, 16
-    .db 2, -1, 16, -2, -1, 32, 2, -1, 16
-    .db 2, -1, 16, -2, -1, 32, 2, -1, 16
-    .db 2, -1, 16, -2, -1, 32, 2, -1, 16
-    .db 2, -1, 24
-    .db SELF_DESTRUCT
-
-  Pattern5: ; Slope.
-    .db SET_DELAY 2
-    .rept 40
-      .db 0, -1, 5,
-      .db 1, -1, 1,
-    .endr
-    .db -1, 1, 20
-    .db SET_DELAY 3
-    .db -2, 1,20
-    .db SET_DELAY 0
-    .db -1,1,140
-    .db SELF_DESTRUCT
-
-  Pattern6: ; Go straight left...
-    .db SET_DELAY 4
-    .db 0,-1,250
-    .db 0,-1,50
-    .db SELF_DESTRUCT
-
-  SwabbyInitString:
-    .db 1                     ; Initial status.
-    .db 20 100                ; Start Y and start X.
-    .dw Swabby1               ; MetaSpritePointer.
-    .db JOYSTICK_1 2 2        ; Movement type, vertical and horizontal speed.
-    .dw SwabbyFlying          ; Animation table base address.
-    .db 0 0                   ; Animation table index and timer.
-    .dw $0000                 ; Movement pattern table.
-    .db 0                     ; Pattern timer.
-    .db 0,0                   ; Delay counter and delay value.
-    .db NO_MASKS              ; MetaSprite flags.
-    .db 0                     ; Self destruct.
-
-  GargoyleFlying:
-    .dw $0700 Gargoyle1 $07ff Gargoyle2
-
-  GargoyleInitString:
-    .db 1
-    .db 94 16
-    .dw Gargoyle1
-    .db PATTERN 1 1
-    .dw GargoyleFlying
-    .db 0 0
-    .dw Pattern4
-    .db 0
-    .db 3,3
-    .db LEFT_MASK             ; Hard enable left mask.
-    .db 0
-
-  ZombieHeadBanging:
-    .dw $2f00 Zombie1 $2fff Zombie2
-
-  ZombieInitString:
-    .db 1
-    .db 160 16
-    .dw Zombie1
-    .db PATTERN, 0, -1
-    .dw ZombieHeadBanging
-    .db 0,0
-    .dw Pattern6
-    .db 0
-    .db 1,1
-    .db LEFT_MASK
-    .db 0
 
   WaveScript1:
     .dw 10                    ; Timer (count down to next wave).
     .db 70, 4                 ; Interval between enemies in wave, number of en.
     .dw GargoyleInitString    ; Init string for the enemies.
-    .dw 600
-    .db 0, 1
-    .dw ZombieInitString
     ; ----
     .dw $ffff                 ; End of wavescript marker.
-
 
   SetupMain:
     ld a,0
